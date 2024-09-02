d671eda92019387f4fd8ec389f0db009b79115d6
Mauri Gon <gonmauri89@gmail.com>
2024-09-02 02:38:59 +0000

    subida total

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io", ".es"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < extensions.length; e++) {
+          let domain = pronoun[p] + adj[a] + noun[n] + extensions[e];
+          console.log(domain);
+        }
+
+        let nounLength = noun[n].length;
+        for (let e = 0; e < extensions.length; e++) {
+          let ext = extensions[e];
+          let hackIndex = nounLength - ext.length + 1;
+          if (hackIndex > 0 && noun[n].endsWith(ext.substring(1))) {
+            let hackedDomain =
+              pronoun[p] + adj[a] + noun[n].substring(0, hackIndex) + ext;
+            console.log(hackedDomain);
+          }
+        }
+      }
+    }
+  }
 };
